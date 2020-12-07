4c2acb52d7747ab32a6cd24d991f175bb32d92d2
Clare Jolly <clajollyuk@gmail.com>
2020-12-07 01:34:46 +0000

    minor changes

--- template/index.test.js
+++ template/index.test.js
@@ -3,6 +3,6 @@ import run from '.';
 describe('Run all', () => {
   it('returns the expected values', () => {
     const result = run();
-    expect(result).toEqual({ part1: 989824, part2: 66432240 });
+    expect(result).toEqual({ part1: undefined, part2: undefined });
   });
 });
