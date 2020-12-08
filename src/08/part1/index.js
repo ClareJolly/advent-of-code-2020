2442febee17b4a19ea60a3b2953cbfd5944c9ddb
Clare Jolly <clare.jolly@vodafone.com>
2020-12-08 10:47:37 +0000

    day 8 part 1 done

--- src/08/part1/index.js
+++ src/08/part1/index.js
@@ -1,3 +1,44 @@
-const part1 = (inputData) => {};
+const part1 = (inputData) => {
+  const testData = [
+    'nop +0',
+    'acc +1',
+    'jmp +4',
+    'acc +3',
+    'jmp -3',
+    'acc -99',
+    'acc +1',
+    'jmp -4',
+    'acc +6',
+  ]
 
-export default part1;
+  const data = inputData
+    .map((d) => d.split(' '))
+    .map((dd) => [dd[0], parseInt(dd[1])])
+
+  let accumulator = 0
+  let runNext = 0
+  let runList = []
+  const run = (runNext) => {
+    runList.push(runNext)
+    if (data[runNext][0] === 'nop') {
+      runNext++
+      if (runList.length && runList.includes(runNext)) return
+      run(runNext)
+    }
+    if (data[runNext][0] === 'acc') {
+      if (runList.length && runList.includes(runNext + 1)) return
+      accumulator += data[runNext][1]
+      runNext++
+      run(runNext)
+    }
+    if (data[runNext][0] === 'jmp') {
+      runNext += data[runNext][1]
+      if (runList.length && runList.includes(runNext)) return
+      run(runNext)
+    }
+  }
+  run(runNext)
+  return accumulator
+}
+
+export default part1
