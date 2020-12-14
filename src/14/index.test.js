8b6a5b5dec4761a760bda032e6e643334c5decc1
Clare Jolly <clarejollyuk@gmail.com>
2020-12-14 21:36:35 +0000

    day 14 part 2 done

--- src/14/index.test.js
+++ src/14/index.test.js
@@ -3,6 +3,6 @@ import run from '.';
 describe('Run all', () => {
   it('returns the expected values', () => {
     const result = run();
-    expect(result).toEqual({ part1: 8570568288597, part2: undefined });
+    expect(result).toEqual({ part1: 8570568288597, part2: 3289441921203 });
   });
 });
