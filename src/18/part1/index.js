833bb1efb7a0f688ebf5d30af3bf3d44864f4ab2
Clare Jolly <clare.jolly@vodafone.com>
2020-12-18 16:13:08 +0000

    day 18 part 1 done

--- src/18/part1/index.js
+++ src/18/part1/index.js
@@ -1,3 +1,36 @@
-const part1 = (inputData) => {};
+const operators = {
+  '+': function (a, b) {
+    return a + b
+  },
+  '*': function (a, b) {
+    return a * b
+  },
+}
 
-export default part1;
+const process = (item) => {
+  if (!isNaN(Number(item))) {
+    Number(item)
+  }
+  while (item.match(/\(/)) {
+    item = item.replace(/\([^()]+\)/, (match) =>
+      process(match.slice(1, match.length - 1))
+    )
+  }
+  while (isNaN(Number(item))) {
+    item = item.replace(/(\d+) ([+*]) (\d+)/, (match, a, op, b) =>
+      operators[op](parseInt(a), parseInt(b))
+    )
+  }
+  return Number(item)
+}
+
+const part1 = (inputData) => {
+  let total = 0
+  inputData.forEach((c) => {
+    total += process(c)
+  })
+
+  return total
+}
+
+export default part1
